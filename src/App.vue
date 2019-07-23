7d442d13516695d12fa5b066e2522ee3f28e4a28
정표용 <sur2@naver.com>
2019-07-23 09:49:37 +0900

    email login

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light" onclick="location.href='/'">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="/login"
+      >
+      <!-- v-btn 클릭 시 새 창 띄우기 : target="_blank" -->
+        <span class="mr-2">Login</span>
+      </v-btn>
+
+    </v-toolbar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <v-layout
+    text-xs-center
+    wrap
+    >
+    <v-flex xs12>
+      <v-img
+      :src="require('../assets/logo.svg')"
+      class="my-3"
+      contain
+      height="200"
+      ></v-img>
+    </v-flex>
+
+    <v-flex mb-4>
+      <h1 class="display-2 font-weight-bold mb-3">Sign In</h1>
+
+      <v-text-field>
+        <template v-slot:label>Email</template>
+      </v-text-field>
+
+      <v-text-field type='password'>
+        <template v-slot:label>Password</template>
+      </v-text-field>
+
+      <v-btn>Connection</v-btn>
+      <p class="subheading font-weight-regular">You don't hava an account ? You can create one</p>
+    </v-flex>
+  </v-layout>
+</v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.v-text-field{
+  margin: auto;
+  width:50%;
+}
+</style>
